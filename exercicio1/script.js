bc6f48f9fca3637a1e1ba571fd525795c97e539a
Juliana <juliana-marques-@hotmail.com>
2022-08-02 18:51:53 -0300

    exercício realizado

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+const sacolao =[]
+
+const fruta1 = {
+    nome: 'melancia',
+    preco: 20.00,
+    disponivel: true
+}
+ const fruta2 = {
+    nome: 'mamão',
+    preco: 10.00,
+    disponivel: false
+}
+const fruta3 = {
+    nome: 'melão',
+    preco: 15.00,
+    disponivel: true
+}
+
+sacolao.push(fruta1,fruta2,fruta3)
+
+console.log(sacolao)
---
+++ exercicio3/script.js
@@ -0,0 +1,54 @@
+const pokemon1 = {
+	nome: "Bulbasaur",
+	tipo: "Grama",
+	nivel: 5
+}
+
+//a)
+
+const pokemon2= {...pokemon1,
+    nome: "Squirtle",
+    tipo: "Água"
+}
+
+//b)
+
+pokemon1.ataques1 = []
+
+const ataque = {
+    nome: "Investida",
+    dano: 40,
+    tipo: "Normal",
+    precisao: 100    
+}
+
+pokemon1.ataques1.push(ataque)
+
+
+//c)
+
+pokemon2.ataques1 =  pokemon1.ataques1
+
+
+//d)
+
+pokemon1.ataques2 = [{
+    nome: "Folha Navalha",
+    dano: 45,
+    tipo: "Grama",
+    precisao: 100
+}]
+
+//e)
+
+pokemon2.ataques1 = [{              //o exercício pedia para"criar" o ataque
+    nome: "Jato de Água",           //isso é o mesmo que "adicionar"?
+    dano: 40,
+    tipo: "Água",
+    precisao: 100
+}]
+
+//f)
+
+console.log(pokemon1)
+console.log(pokemon2)
